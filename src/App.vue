fc21cfcf8abf0ee6353f12b4884274df3bf9fcb0
samuel <53528911+samuel-jimenez@users.noreply.github.com>
2024-01-11 13:07:28 -0600

    hello

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+	Hello WOrld!
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
